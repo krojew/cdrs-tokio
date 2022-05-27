007ae1ad47444193a42c74e02b33d74d8d09514d
conorbros <conor.brosnan@instaclustr.com>
2022-05-27 17:58:55 +1000

    make logs less noisy

--- cdrs-tokio/src/cluster/node_info.rs
+++ cdrs-tokio/src/cluster/node_info.rs
@@ -1,15 +1,18 @@
 use cassandra_protocol::token::Murmur3Token;
+use derivative::Derivative;
 use derive_more::Constructor;
 use std::net::SocketAddr;
 use uuid::Uuid;
 
 /// Information about a node.
-#[derive(Debug, Constructor, Clone)]
+#[derive(Constructor, Clone, Derivative)]
+#[derivative(Debug)]
 pub struct NodeInfo {
     pub host_id: Uuid,
     pub broadcast_rpc_address: SocketAddr,
     pub broadcast_address: Option<SocketAddr>,
     pub datacenter: String,
+    #[derivative(Debug = "ignore")]
     pub tokens: Vec<Murmur3Token>,
     pub rack: String,
 }
